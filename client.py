f96e5d2c4d28f44c6e86d7699526310dafefafd5
Noah Thornton <113550977+nthornton00@users.noreply.github.com>
2023-04-14 20:28:29 -0700

    Update client.py

--- client.py
+++ client.py
@@ -1,15 +1,47 @@
 from socket import *
+from threading import Thread
+import time
+from datetime import datetime
+
 serverName = 'localhost'
 serverPort = 18000
+
+# TCP
 clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName,serverPort))
+clientSocket.connect((serverName, serverPort))
+print("Connection Successful.")
+print("Type q to exit chatroom")
+
 verify = clientSocket.recv(1024)
 address = clientSocket.recv(1024)
 print(verify.decode())
 print(address)
-while(1):
-        sentence = input('Enter username')
+
+username = input("Enter your a username: ")
+
+
+def listen_chat():
+    while True:
+        chat = clientSocket.recv(1024).decode()
+        print("\n" + chat)
+
+
+temp_thread = Thread(target=listen_chat)
+temp_thread.daemon = True
+temp_thread.start()
+
+while True:
+    sentence = input()
+
+    if sentence.lower() == "q":
         clientSocket.send(sentence.encode())
-        modifiedSentence = clientSocket.recv(1024)
-        print ('From Server:', modifiedSentence.decode())
+        break
+
+    sentence = username + ": " + sentence
+    curr_date = datetime.now().strftime("[%H:%M] ")
+    sentence = curr_date + sentence
+
+    clientSocket.send(sentence.encode())
+
+# close clientSocket
 clientSocket.close()
