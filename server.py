5498f162c66da2a701dc679b1d2b88d4ceffe4c2
Josh Larson <113719434+JoshyLar@users.noreply.github.com>
2023-04-14 20:20:25 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,18 +1,45 @@
-from socket import *
-users = {}
-serverPort = 18000
-serverSocket = socket(AF_INET,SOCK_STREAM)
-serverSocket.bind(('',serverPort))
-serverSocket.listen(1)
-print ('The server is ready to receive')
-while 1:
-    connectionSocket, addr = serverSocket.accept()
-    connectionSocket.send('succesfull server connection!\n'.encode())
-    user_name = connectionSocket.recv(1024).decode()
-    users[addr] = user_name, port
-    print(users)
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence.encode())
-connectionSocket.close()
+import socket
+from threading import Thread
 
 
+
+
+
+def listen(client):
+ while True:
+    msg = client.recv(1024).decode()
+    for connectionSocket in users:
+        connectionSocket.send(msg.encode())
+
+
+if __name__ == '__main__':
+    users = set()
+
+    server_port = 18001
+    host_name = 'local host'
+
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    name = socket.gethostname()
+    server_ip = socket.gethostbyname(name)  # get the server ip
+    serverSocket.bind(('', server_port))
+
+    print("server IP= ", server_ip, "port= ", server_port)
+    print('The server is ready to receive')
+    serverSocket.listen(3)
+
+    while True:
+        connectionSocket, addr = serverSocket.accept()
+        connectionSocket.send('succesfull server connection!\n'.encode())
+        users.add(connectionSocket)
+
+
+        thread = Thread(target=listen, args = connectionSocket)
+
+        thread.daemon = True
+
+        thread.start()
+
+    for connectionSocket in users:
+        connectionSocket.close()
+
+    serverSocket.close()
