febb40f65533dc75dca32cdaedacdb2725b9508d
Josh Larson <113719434+JoshyLar@users.noreply.github.com>
2023-04-14 21:33:22 -0700

    Update server.py

--- server.py
+++ server.py
@@ -6,16 +6,38 @@ from threading import Thread
 
 
 def listen(client):
+ string = "REPORT_REQUEST_FLAG=1\n" + "number of users: " + str(len(users)) + "\n"
+ for i in users:
+    string = string + "ip = " + str(i[1]) + ", port= " + str(i[2]) + ", user name= " + i[3] + "\n"
+ print(string)
  while True:
     msg = client.recv(1024).decode()
+
+    try:
+        if msg == "REPORT_REQUEST_FLAG=1":
+            string = "REPORT_REQUEST_FLAG=1\n" + "number of users" + str(len(users))+ "\n"
+            for i in users:
+                string = string + "ip = " + str(i[1]) + " port= " + str(i[2]) + "user name= " + i[3] + "\n"
+            print(string)
+            connectionSocket.send("hey".encode())
+
+        if msg == "q":
+            print("Client Disconnected")
+            users.remove(connectionSocket)
+            connectionSocket.close()
+
+    except:
+
+        print("Error")
+        users.remove(connectionSocket)
     for connectionSocket in users:
         connectionSocket.send(msg.encode())
 
 
 if __name__ == '__main__':
-    users = set()
+    users = []
 
-    server_port = 18001
+    server_port = 18002
     host_name = 'local host'
 
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -29,11 +51,14 @@ if __name__ == '__main__':
 
     while True:
         connectionSocket, addr = serverSocket.accept()
+        IP,Port = addr
         connectionSocket.send('succesfull server connection!\n'.encode())
-        users.add(connectionSocket)
+        user_name = connectionSocket.recv(1024).decode()
+        users.append((connectionSocket,IP,Port,user_name))
 
 
-        thread = Thread(target=listen, args = connectionSocket)
+
+        thread = Thread(target=listen, args = (connectionSocket,))
 
         thread.daemon = True
 
